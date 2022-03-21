53a550d7b974a7492264625f99206dcb156b8e3e
Saptak Dutta <saptakdutta@cmail.carleton.ca>
2022-03-21 11:24:10 -0400

    Major structural changes

--- irradiance.py
+++ irradiance.py
@@ -2,22 +2,16 @@
 import pandas as pd, numpy as np, requests as req, json, tqdm
 from pathlib import Path
 from scipy.interpolate import interp1d, interp2d
+from ConnLib.connector import cleanup, listToString, connector
 
 #%% Set working path directory
 cwd = Path.cwd()
 path = cwd.__str__()
 
-# Define Functions
-# Function to convert list to str
-def listToString(s):
-    listToStr = ','.join(map(str, s))
-    # return string
-    return listToStr
-
 #Grab the locations
 locations_file = 'locations.json'
 #load in the json file
-with open(path+'/'+locations_file) as f:
+with open(path+'/Config/'+locations_file) as f:
   locations = json.load(f)
 #Now only keep the locations which you want data for
 for location in list(locations):
@@ -25,7 +19,8 @@ for location in list(locations):
         locations.pop(location,None)
 
 # Grab the required parameters
-pars = pd.read_csv(path+'/parameters.csv')
+parameters_file = 'parameters.csv'
+pars = pd.read_csv(path+'/Config/'+parameters_file)
 pars = pars[pars['Use'] == 'Yes']
 params = listToString(pars["Parameter"].to_list())
 
@@ -35,22 +30,16 @@ timeformat = 'UTC'
 community = 'RE'
 # Start/end time in format: 'YYYYMMDD'
 sTime = '20200101'
-eTime = '20200705'
+eTime = '20200201'
 
 #%% API call for given location
 for location in tqdm.tqdm(locations):
-    url = 'https://power.larc.nasa.gov/api/temporal/hourly/point?Time='+timeformat+'&parameters='+params+'&community='+community+'&longitude='+locations[location]['Longitude']+'&latitude='+locations[location]['Latitude']+'&start='+sTime+'&end='+eTime+'&format=JSON'
-    solar = req.get(url)
-    solar = solar.json()
+    solar = connector(timeformat, params, community, locations[location]['Longitude'], locations[location]['Latitude'], sTime, eTime)
     #Isolate only the required solar parameter
     data = solar['properties']['parameter']['ALLSKY_SFC_SW_DWN']
     solar_irr = pd.DataFrame({'Time':data.keys(),
                               'Solar Irradiation':data.values()})
-    solar_irr['Time'] = pd.to_datetime(solar_irr['Time'], format = '%Y%m%d%H')
-    solar_irr = solar_irr.set_index('Time').resample('15min').ffill()
-    solar_irr['Solar Irradiation'] = solar_irr['Solar Irradiation'].astype(float)
-    solar_irr = solar_irr.reset_index()
-    solar_irr['Time'] = solar_irr['Time'].dt.tz_localize('UTC').dt.tz_convert('EST')
+    cleanup(solar_irr,'Time','Solar Irradiation')
     solar_irr.to_csv(path+'/Data/{}_Solar.csv'.format(location), index=False)
 
 # %%
