714739e5129363c5587fa8f1d9a0144fc2290662
Saptak Dutta <saptakdutta@cmail.carleton.ca>
2022-03-21 13:05:08 -0400

    minor edits

--- irradiance.py
+++ irradiance.py
@@ -1,31 +1,23 @@
-#%% Libraries
-import pandas as pd, numpy as np, requests as req, json, tqdm
+# Libraries
+import pandas as pd, numpy as np, requests as req, json, tqdm, sys
 from pathlib import Path
 from Library.connector import listToString, connector
 
-#%% Set working path directory
+# Set working path directory
 cwd = Path.cwd()
 path = cwd.__str__()
-
 # Identify correct environment 
+print('The downloader is currently using the following venv: {} \n'.format(sys.executable))
 
-
-#Grab the locations
+# Grab the locations file
 locations_file = 'locations.json'
-#load in the json file
+# load in the json file
 with open(path+'/Config/'+locations_file) as f:
   locations = json.load(f)
-#Now only keep the locations which you want data for
+# Now only keep the locations which you want data for
 for location in list(locations):
     if (locations[location]['Use'] == 'No'):
         locations.pop(location,None)
-
-# Grab the required parameters we want to download from POWER
-parameters_file = 'parameters.csv'
-pars = pd.read_csv(path+'/Config/'+parameters_file)
-#Only grab parameters we have selected as 'Yes'
-pars = pars[pars['Use'] == 'Yes']
-params = listToString(pars["Parameter"].to_list())
 
 # Grab Runtime parameters
 runtime_file = 'runtime.json'
@@ -39,11 +31,16 @@ community = runtime['communities']
 sTime = runtime['sTime']
 eTime = runtime['eTime']
 
-#%% API call for given location
+# Grab the required parameters we want to download from POWER
+parameters_file = 'parameters.csv'
+pars = pd.read_csv(path+'/Config/'+parameters_file)
+# Only grab parameters we have selected as 'Yes'
+pars = pars[pars['Use'] == 'Yes']
+params = listToString(pars["Parameter"].to_list())
+
+# API call for given location
 print('Downloading data: \n')
 for location in tqdm.tqdm(locations):
     solar = connector(timeformat, params, community, locations[location]['Longitude'], locations[location]['Latitude'], sTime, eTime)
     solar_irr = pd.DataFrame(solar['properties']['parameter'])
     solar_irr.to_csv(path+'/Data/{}_Solar.csv'.format(location))
-
-# %%
